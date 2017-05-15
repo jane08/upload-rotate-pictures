72007d9546b5b3534cb3c39b1f3bc826a1e1c78b
jane <you@example.com>
2017-05-15 08:18:31 +0300

    fixed bug

--- common/models/PictureSearch.php
+++ common/models/PictureSearch.php
@@ -6,6 +6,7 @@ use Yii;
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
 use common\models\Picture;
+use yii\web\ForbiddenHttpException;
 
 /**
  * PictureSearch represents the model behind the search form about `common\models\Picture`.
